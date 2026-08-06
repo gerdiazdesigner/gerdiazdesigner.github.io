86dfa9c4f59bf384b907889c34ac624bf4b726d7
gerdiazdesigner <gerpdisenador@gmail.com>
2026-08-06 11:38:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -121,6 +121,8 @@ prevButton.addEventListener("click", async () => {
 // =========================
 
 book.addEventListener("wheel", async (event) => {
+
+    console.log("wheel");
     
     event.preventDefault();
 
